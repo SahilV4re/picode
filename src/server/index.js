ee537c79820a36ade15086752f7b4a15655b0147
SahilV4re <vicharesahil14@gmail.com>
2025-03-28 21:51:56 +0530

    rr

--- src/server/index.js
+++ src/server/index.js
@@ -9,9 +9,3 @@ server.listen(5000, () => {
     console.log(`server running on port ${port} `)
 })
 
-server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
